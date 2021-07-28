18f2e607476a1535268da1b29178fb4bd4dcaf77
jihwanKim <jihwan.dev@gmail.com>
2021-07-29 00:06:51 +0900

    총시간이 모든 여행지 필요시간 합친거보다 클경우, 모든 요소 바로 반환하도록 수정.

--- first.js
+++ first.js
@@ -1,4 +1,20 @@
+function getTotalRequireTime(allCourses){
+  let totalTime = 0;
+  for(let i = 0 ; i < allCourses.length; i++){
+    totalTime+= allCourses[i].requireTime;
+  }
+  return totalTime;
+}
+
 function getAllCase(haveTime, allCourses){
+  const totalTime = getTotalRequireTime(allCourses);
+  if(totalTime <= haveTime){
+    const checkList = [];
+    for(let i = 0 ; i < allCourses.length; i++){
+      checkList.push(i);
+    }
+    return [checkList];
+  }
 
   const lastValue = parseInt(Math.pow(2, allCourses.length));
   const totalCheckList = {};
@@ -32,7 +48,6 @@ function getMaximumSatisFaction(haveTime, allCourses){
   allCourses.sort((elem1, elem2)=> elem1.requireTime > elem2.requireTime ? 1 : -1);
 
   const allCase = getAllCase(haveTime, allCourses);
-
 
   let maxSatisfaction = 0;
 
@@ -68,4 +83,27 @@ console.log(getMaximumSatisFaction(5, [
   {name:"도톤보리", requireTime:2, satisfaction:16},
   {name:"도톤보리", requireTime:2, satisfaction:14},
   {name:"오사카성", requireTime:3, satisfaction:18},])
+);
+
+console.log(getMaximumSatisFaction(1000, [
+  {name:"유니버설스튜디오", requireTime:1, satisfaction:1},
+  {name:"유니버설스튜디오", requireTime:1, satisfaction:2},
+  {name:"도톤보리", requireTime:2, satisfaction:3},
+  {name:"도톤보리", requireTime:2, satisfaction:4},
+  {name:"오사카성", requireTime:3, satisfaction:5},
+  {name:"유니버설스튜디오", requireTime:1, satisfaction:6},
+  {name:"유니버설스튜디오", requireTime:1, satisfaction:7},
+  {name:"도톤보리", requireTime:2, satisfaction:8},
+  {name:"도톤보리", requireTime:2, satisfaction:9},
+  {name:"오사카성", requireTime:3, satisfaction:10},
+  {name:"유니버설스튜디오", requireTime:1, satisfaction:11},
+  {name:"유니버설스튜디오", requireTime:1, satisfaction:12},
+  {name:"도톤보리", requireTime:2, satisfaction:13},
+  {name:"도톤보리", requireTime:2, satisfaction:14},
+  {name:"오사카성", requireTime:3, satisfaction:15},
+  {name:"유니버설스튜디오", requireTime:1, satisfaction:16},
+  {name:"유니버설스튜디오", requireTime:1, satisfaction:17},
+  {name:"도톤보리", requireTime:2, satisfaction:18},
+  {name:"도톤보리", requireTime:2, satisfaction:19},
+  {name:"오사카성", requireTime:3, satisfaction:20},])
 );
